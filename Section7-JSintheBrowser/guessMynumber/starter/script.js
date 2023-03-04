403deb368504450391bb4835e2774ce01aef0d52
Stephen Dunning <madguy69@gmail.com>
2023-03-03 23:19:53 -0300

    Various changes trying to get the again button to work

--- Section7-JSintheBrowser/guessMynumber/starter/script.js
+++ Section7-JSintheBrowser/guessMynumber/starter/script.js
@@ -97,11 +97,11 @@ document.querySelector('.check').addEventListener('click', function () {
       secretNumber = Math.trunc(Math.random() * 20) + 1;
 
       document.querySelector('.message').textContent = 'Start guessing...';
-      document.querySelector('.score').textContent = 'score:';
+      document.querySelector('.score').textContent = score;
       document.querySelector('.number').textContent = '?';
       document.querySelector('.guess').value = '';
 
-      document.querySelector('.body').style.backgroundColor = '#222';
+      document.querySelector('body').style.backgroundColor = '#222';
       document.querySelector('.number').style.width = '15rem';
     };
 });
