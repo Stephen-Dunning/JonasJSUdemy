6eef1897f7b2a98c00c53e5ae2d4dfc263673847
Stephen Dunning <madguy69@gmail.com>
2023-03-14 22:40:59 -0300

    Initial lesson 79 Modal View

--- Section7-JSintheBrowser/06-Modal/starter/script.js
+++ Section7-JSintheBrowser/06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+// lecture 79 Project 2 Intro - Modal WIndow 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelector('.open-modal');
+console.log(btnsOpenModal);
+
+  for (let i = 0; i  < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
