150697eca58e13324348cf33b4f36f828528f8c8
Stephen Dunning <madguy69@gmail.com>
2023-04-18 23:13:38 -0300

    dice connectivity

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new'); 
+const btnRoll = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--new');
 
 //starting conditions
 score0El.textContent = 0;
