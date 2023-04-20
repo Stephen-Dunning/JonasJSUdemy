c2136289c61a2365062ddabbd98820ab36fea0d7
Stephen Dunning <madguy69@gmail.com>
2023-04-19 22:32:59 -0300

    dice roll functionality added

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ const btnHold = document.querySelector('.btn--new');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function() {
+//1. generating a random dice roll
+
+
+// 2. display dice
+
+//check for rolled 1: if true, switch to next player
+})
